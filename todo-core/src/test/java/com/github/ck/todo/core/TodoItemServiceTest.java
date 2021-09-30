3636d871a044281b6b2704b20f8a7ade04413c15
chengkunxf <chengkunxf@126.com>
2021-09-30 16:55:56 +0800

    feature: add mock todoItemRepository

--- todo-core/src/test/java/com/github/ck/todo/core/TodoItemServiceTest.java
+++ todo-core/src/test/java/com/github/ck/todo/core/TodoItemServiceTest.java
@@ -2,6 +2,10 @@ package com.github.ck.todo.core;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * @author chengkunxf@126.com
@@ -11,7 +15,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class TodoItemServiceTest {
     @Test
     public void should_add_todo_item() {
-        TodoItemRepository repository = null;
+        TodoItemRepository repository = mock(TodoItemRepository.class);
+        when(repository.save(any())).then(returnsFirstArg());
         TodoItemService service = new TodoItemService(repository);
         TodoItem todoItem = service.addTodoItem(new TodoParameter("foo"));
         assertThat(todoItem.getContent()).isEqualTo("foo");
