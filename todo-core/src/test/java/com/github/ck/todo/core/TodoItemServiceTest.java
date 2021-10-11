e4830932b93fa175cf2fa320f77df67e892d198f
chengkunxf <chengkunxf@126.com>
2021-10-11 18:20:30 +0800

    feature: 完善 should_list_all 单元测试

--- todo-core/src/test/java/com/github/ck/todo/core/TodoItemServiceTest.java
+++ todo-core/src/test/java/com/github/ck/todo/core/TodoItemServiceTest.java
@@ -66,9 +66,13 @@ public class TodoItemServiceTest {
     public void should_list_all() {
         TodoItem foo = new TodoItem("foo");
         foo.assignIndex(1);
-        when(repository.findAll()).thenReturn(ImmutableList.of(foo));
+        foo.markDone();
+        TodoItem bar = new TodoItem("bar");
+        bar.assignIndex(2);
+        when(repository.findAll()).thenReturn(ImmutableList.of(foo, bar));
+
         List<TodoItem> list = service.list(true);
-        assertThat(list).hasSize(1);
+        assertThat(list).hasSize(2);
         TodoItem todoItem = list.get(0);
         assertThat(todoItem.getContent()).isEqualTo("foo");
         assertThat(todoItem.getIndex()).isEqualTo(1);
