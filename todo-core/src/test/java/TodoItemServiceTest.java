69245c873edb931902a860b0c849d1452d9410a9
chengkunxf <chengkunxf@126.com>
2021-10-11 16:22:19 +0800

    feature: 先写伪代码

---
+++ todo-core/src/test/java/TodoItemServiceTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author chengkunxf@126.com
+ * @date 2021/10/11 4:19 下午
+ * @description
+ */
+public class TodoItemServiceTest {
+
+    @Test
+    public void should_add_todo_item(){
+        TodoItemRepository repository = null;
+        TodoItemService service = new TodoItemService(repository);
+        TodoItem todoItem = service.addTodoItem(new TodoParameter("foo"));
+        assertThat(todoItem.getContent()).isEqualTo("foo");
+    }
+}
