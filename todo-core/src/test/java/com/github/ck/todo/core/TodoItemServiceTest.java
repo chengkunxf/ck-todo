cf393d9f3ce9787493f7c111fd3c3f401a535209
chengkunxf <chengkunxf@126.com>
2021-10-11 17:12:45 +0800

    refactor: 重构，抽取setup

--- todo-core/src/test/java/com/github/ck/todo/core/TodoItemServiceTest.java
+++ todo-core/src/test/java/com/github/ck/todo/core/TodoItemServiceTest.java
@@ -1,6 +1,7 @@
 package com.github.ck.todo.core;
 
 import com.google.common.collect.ImmutableList;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,35 +17,38 @@ import static org.mockito.Mockito.when;
  */
 public class TodoItemServiceTest {
 
+    private TodoItemRepository repository;
+    private TodoItemService service;
+
+    @BeforeEach
+    private void setUp(){
+        repository = mock(TodoItemRepository.class);
+        service = new TodoItemService(repository);
+    }
+
     @Test
     public void should_add_todo_item() {
-        TodoItemRepository repository = mock(TodoItemRepository.class);
         when(repository.save(any())).then(returnsFirstArg());
-        TodoItemService service = new TodoItemService(repository);
         TodoItem todoItem = service.addTodoItem(new TodoParameter("foo"));
         assertThat(todoItem.getContent()).isEqualTo("foo");
     }
 
     @Test
     public void should_mark_todo_item_as_done(){
-        TodoItemRepository repository = mock(TodoItemRepository.class);
         when(repository.save(any())).then(returnsFirstArg());
         TodoItem foo = new TodoItem("foo");
         foo.assignIndex(1);
         when(repository.findAll()).thenReturn(ImmutableList.of(foo));
-        TodoItemService service = new TodoItemService(repository);
         TodoItem todoItem = service.markTodoItemDone(new TodoIndexParameter(1));
         assertThat(todoItem.isDone()).isTrue();
     }
 
     @Test
     public void should_not_mark_todo_item_as_done_for_out_of_index(){
-        TodoItemRepository repository = mock(TodoItemRepository.class);
         when(repository.save(any())).then(returnsFirstArg());
         TodoItem foo = new TodoItem("foo");
         foo.assignIndex(1);
         when(repository.findAll()).thenReturn(ImmutableList.of(foo));
-        TodoItemService service = new TodoItemService(repository);
         TodoItem todoItem = service.markTodoItemDone(new TodoIndexParameter(2));
         assertThat(todoItem).isNull();
     }
