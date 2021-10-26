f529c34f5d45d69fbf6e629e0c05c6f2358d9e3c
chengkunxf <chengkunxf@126.com>
2021-10-26 11:33:23 +0800

    fix: 增加异常测试

--- todo-cli/src/test/java/com/github/ck/todo/cli/TodoCommandTest.java
+++ todo-cli/src/test/java/com/github/ck/todo/cli/TodoCommandTest.java
@@ -50,12 +50,19 @@ class TodoCommandTest {
     }
 
     @Test
-    public void mark_todo_item_done() {
+    public void should_mark_todo_item_done() {
         service.addTodoItem(new TodoParameter("foo"));
         cli.execute("done", "1");
 
         final List<TodoItem> items = service.list(true);
         assertThat(items).hasSize(1);
         assertThat(items.get(0).getContent()).isEqualTo("foo");
+    }
+
+    @Test
+    public void should_mark_todo_done_by_wrong_index() {
+        service.addTodoItem(new TodoParameter("foo"));
+        assertThat(cli.execute("done", "-1")).isNotEqualTo(0);
+        assertThat(cli.execute("done", "2")).isNotEqualTo(0);
     }
 }
