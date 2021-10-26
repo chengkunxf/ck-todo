eeb5f8109049e0e6ef1f0acc9fdde714850feb13
chengkunxf <chengkunxf@126.com>
2021-10-26 11:28:07 +0800

    feature: 完成done

--- todo-cli/src/main/java/com/github/ck/todo/cli/TodoCommand.java
+++ todo-cli/src/main/java/com/github/ck/todo/cli/TodoCommand.java
@@ -1,10 +1,13 @@
 package com.github.ck.todo.cli;
 
+import com.github.ck.todo.service.TodoIndexParameter;
 import com.github.ck.todo.service.TodoItem;
 import com.github.ck.todo.service.TodoItemService;
 import com.github.ck.todo.service.TodoParameter;
 import com.google.common.base.Strings;
 import picocli.CommandLine;
+
+import java.util.Optional;
 
 /**
  * @author chengkunxf@126.com
@@ -31,6 +34,20 @@ public class TodoCommand {
         TodoItem todoItem = service.addTodoItem(new TodoParameter(item));
         System.out.printf("%d. %s%n", todoItem.getIndex(), todoItem.getContent());
         System.out.printf("Item <%d> added%n", todoItem.getIndex());
+        return 0;
+    }
+
+    @CommandLine.Command(name = "add")
+    public int done(@CommandLine.Parameters(index = "0") final int index) {
+        if (index <= 0) {
+            throw new CommandLine.ParameterException(spec.commandLine(), "index should be greater than 0");
+        }
+        Optional<TodoItem> optionalTodoItem = service.markTodoItemDone(new TodoIndexParameter(index));
+        if (!optionalTodoItem.isPresent()) {
+            throw new CommandLine.ParameterException(spec.commandLine(), "unknow todo item index");
+        }
+        final TodoItem actual = optionalTodoItem.get();
+        System.out.printf("Item <%d> done%n", actual.getIndex());
         return 0;
     }
 
