5e537ebe7c44ad42ad5e65022360c65b30c9e2fc
chengkunxf <chengkunxf@126.com>
2021-10-26 15:07:26 +0800

    feature: 完成add

---
+++ todo-cli/src/main/java/com/github/ck/todo/cli/TodoCommand.java
@@ -0,0 +1,31 @@
+package com.github.ck.todo.cli;
+
+import com.github.ck.todo.core.TodoItem;
+import com.github.ck.todo.core.TodoItemService;
+import com.github.ck.todo.core.TodoParameter;
+import picocli.CommandLine;
+
+/**
+ * @author chengkunxf@126.com
+ * @date 2021/10/26 2:48 下午
+ * @description
+ */
+@CommandLine.Command(name = "todo")
+public class TodoCommand {
+
+    @CommandLine.Spec
+    private CommandLine.Model.CommandSpec spec;
+    private TodoItemService service;
+
+    public TodoCommand(final TodoItemService service) {
+        this.service = service;
+    }
+
+    @CommandLine.Command(name = "add")
+    public int add(@CommandLine.Parameters(index = "0") final String item) {
+        TodoItem todoItem = this.service.addTodoItem(new TodoParameter(item));
+        System.out.printf("%d. %s%n", todoItem.getIndex(), todoItem.getContent());
+        System.out.printf("Item <%d> added%n", todoItem.getIndex());
+        return 0;
+    }
+}
---
+++ todo-cli/src/test/java/com/github/ck/todo/cli/TodoCommandTest.java
@@ -0,0 +1,46 @@
+package com.github.ck.todo.cli;
+
+import com.github.ck.todo.cli.repository.FileTodoItemRepository;
+import com.github.ck.todo.core.TodoItem;
+import com.github.ck.todo.core.TodoItemService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import picocli.CommandLine;
+
+import java.io.File;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author chengkunxf@126.com
+ * @date 2021/10/26 2:42 下午
+ * @description
+ */
+public class TodoCommandTest {
+
+    @TempDir
+    File tempDir;
+    private TodoItemService service;
+    private TodoCommand todoCommand;
+    private CommandLine cli;
+
+    @BeforeEach
+    void setUp() {
+        File repositoryFile = new File(tempDir, "repository.json");
+        FileTodoItemRepository repository = new FileTodoItemRepository(repositoryFile);
+        service = new TodoItemService(repository);
+        todoCommand = new TodoCommand(service);
+        cli = new CommandLine(todoCommand);
+    }
+
+    @Test
+    public void should_add_todo_item() {
+        int result = cli.execute("add", "foo");
+        assertThat(result).isEqualTo(0);
+
+        List<TodoItem> list = service.list(true);
+        assertThat(list).hasSize(1);
+    }
+}
