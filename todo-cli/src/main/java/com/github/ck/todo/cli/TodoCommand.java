df88dfb0ec0e6526c5c9bfc5a1f3574267bfd68a
chengkunxf <chengkunxf@126.com>
2021-10-26 15:45:32 +0800

    feature: 完成list

--- todo-cli/src/main/java/com/github/ck/todo/cli/TodoCommand.java
+++ todo-cli/src/main/java/com/github/ck/todo/cli/TodoCommand.java
@@ -7,6 +7,7 @@ import com.github.ck.todo.core.TodoParameter;
 import com.google.common.base.Strings;
 import picocli.CommandLine;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -49,5 +50,29 @@ public class TodoCommand {
         System.out.printf("Item <%d> done%n", todoItem.getIndex());
 
         return 0;
+    }
+
+    @CommandLine.Command(name = "list")
+    public int list(@CommandLine.Option(names = "--all") boolean isAll) {
+        List<TodoItem> todoItems = this.service.list(isAll);
+        todoItems.stream().map(this::formatItem).forEach(System.out::println);
+        if (isAll) {
+            long count = todoItems.stream().filter(TodoItem::isDone).count();
+            System.out.println(formatTotal(todoItems.size(), count));
+        }
+        return 0;
+    }
+
+    private String formatTotal(int size, long doneCount) {
+        return "Total: "
+                + size + "items, "
+                + doneCount + "item done";
+    }
+
+    private String formatItem(TodoItem todoItem) {
+        if (todoItem.isDone()) {
+            String.format("%d. [done] %s", todoItem.getIndex(), todoItem.getContent());
+        }
+        return String.format("%d. %s", todoItem.getIndex(), todoItem.getContent());
     }
 }
