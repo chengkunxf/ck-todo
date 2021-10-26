eeb4118d79b44845062f2075f1596204ff1a899f
chengkunxf <chengkunxf@126.com>
2021-10-26 11:58:08 +0800

    feature: 增加 bootstrap

---
+++ todo-cli/src/main/java/com/github/ck/todo/cli/Bootstrap.java
@@ -0,0 +1,23 @@
+package com.github.ck.todo.cli;
+
+import picocli.CommandLine;
+
+import java.io.File;
+
+public class Bootstrap {
+    public static void main(final String[] args) {
+        final File todoRepository = new File(todoHome(), "repository.json");
+        ObjectFactory factory = new ObjectFactory();
+        final CommandLine commandLine = factory.createCommandLine(todoRepository);
+        commandLine.execute(args);
+    }
+
+    private static File todoHome() {
+        final File homeDirectory = new File(System.getProperty("user.home"));
+        final File todoHome = new File(homeDirectory, ".todo");
+        if (!todoHome.exists()) {
+            todoHome.mkdirs();
+        }
+        return todoHome;
+    }
+}
