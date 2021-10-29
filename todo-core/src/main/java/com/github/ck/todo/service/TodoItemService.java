e72260044695d50324f2d0a156cddc5d29c21943
chengkunxf <chengkunxf@126.com>
2021-10-29 17:15:36 +0800

    feature: 增加 自动装配

--- todo-core/src/main/java/com/github/ck/todo/service/TodoItemService.java
+++ todo-core/src/main/java/com/github/ck/todo/service/TodoItemService.java
@@ -1,5 +1,6 @@
 package com.github.ck.todo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,6 +18,7 @@ public class TodoItemService {
 
     private final TodoItemRepository repository;
 
+    @Autowired
     public TodoItemService(final TodoItemRepository repository) {
         this.repository = repository;
     }
