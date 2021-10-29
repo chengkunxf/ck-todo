4716f187b5f463cffae99354f53093b238b4dfe2
chengkunxf <chengkunxf@126.com>
2021-10-29 11:49:32 +0800

    feature: 完成todoItemRepository 的测试

---
+++ todo-api/src/test/java/com/github/ck/todo/api/repository/TodoItemRepositoryTest.java
@@ -0,0 +1,64 @@
+package com.github.ck.todo.api.repository;
+
+import com.github.ck.todo.service.TodoItem;
+import com.github.ck.todo.service.TodoItemRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author chengkunxf@126.com
+ * @date 2021/10/29 11:43 上午
+ * @description
+ */
+@DataJdbcTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestPropertySource("classpath:test.properties")
+public class TodoItemRepositoryTest {
+
+    @Autowired
+    private TodoItemRepository repository;
+
+    @Test
+    public void should_find_by_nothing_repository() {
+        List<TodoItem> all = this.repository.findAll();
+        assertThat(all).hasSize(0);
+    }
+
+    @Test
+    public void should_save_todo_item() {
+        TodoItem foo = new TodoItem("foo");
+        TodoItem bar = new TodoItem("bar");
+        this.repository.save(foo);
+        this.repository.save(bar);
+
+        List<TodoItem> all = this.repository.findAll();
+        assertThat(all).hasSize(2);
+    }
+
+    @Test
+    public void should_update_todo_item() {
+        TodoItem foo = new TodoItem("foo");
+        TodoItem bar = new TodoItem("bar");
+        this.repository.save(foo);
+        this.repository.save(bar);
+
+        TodoItem newBar = new TodoItem("newBar");
+        newBar.assignIndex(2);
+        newBar.markDone();
+
+        this.repository.save(newBar);
+
+        List<TodoItem> all = this.repository.findAll();
+        TodoItem todoItem = all.get(1);
+        assertThat(todoItem.getIndex()).isEqualTo(2);
+        assertThat(todoItem.getContent()).isEqualTo("newBar");
+        assertThat(todoItem.isDone()).isTrue();
+    }
+}
--- todo-core/src/main/java/com/github/ck/todo/service/TodoItemRepository.java
+++ todo-core/src/main/java/com/github/ck/todo/service/TodoItemRepository.java
@@ -1,5 +1,8 @@
 package com.github.ck.todo.service;
 
+
+import org.springframework.data.repository.Repository;
+
 import java.util.List;
 
 /**
@@ -7,7 +10,7 @@ import java.util.List;
  * @date 2021/10/22 6:02 下午
  * @description
  */
-public interface TodoItemRepository {
+public interface TodoItemRepository extends Repository<TodoItem, Long> {
     TodoItem save(TodoItem todoItem);
 
     List<TodoItem> findAll();
