17e4ebbebd2cf58c0a20cf02dd7493feb10647ef
chengkunxf <chengkunxf@126.com>
2021-10-29 11:36:48 +0800

    feature: 增加数据库配置,完善 table 映射

--- todo-core/src/main/java/com/github/ck/todo/service/TodoItem.java
+++ todo-core/src/main/java/com/github/ck/todo/service/TodoItem.java
@@ -5,6 +5,8 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 /**
  * @author chengkunxf@126.com
  * @date 2021/10/22 6:02 下午
@@ -13,9 +15,16 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+@Entity
+@Table(name = "todo_items")
 public class TodoItem {
+    @Column
     private String content;
+    @Column
     private boolean done;
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int index;
 
 
