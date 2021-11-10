e9e0492f8ec391c8021d2183129cb83abe3b7ae1
chengkunxf <chengkunxf@126.com>
2021-11-10 11:42:33 +0800

    feature: 完成repository的测试。配置好application,test

---
+++ todo-api/src/main/java/com/github/ck/todo/api/repository/TodoItemJpaRepository.java
@@ -0,0 +1,11 @@
+package com.github.ck.todo.api.repository;
+
+import com.github.ck.todo.core.TodoItemRepository;
+
+/**
+ * @author chengkunxf@126.com
+ * @date 2021/11/10 11:30 上午
+ * @description
+ */
+public interface TodoItemJpaRepository extends TodoItemRepository {
+}
