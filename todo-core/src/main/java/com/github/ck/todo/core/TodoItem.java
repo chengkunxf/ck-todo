c0b99d65720cf1fa0482a41d6c96fcb81b8aa2fa
chengkunxf <chengkunxf@126.com>
2021-11-10 11:23:57 +0800

    feature: 完成实体对象的配置

--- todo-core/src/main/java/com/github/ck/todo/core/TodoItem.java
+++ todo-core/src/main/java/com/github/ck/todo/core/TodoItem.java
@@ -5,6 +5,13 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
 /**
  * @author chengkunxf@126.com
  * @date 2021/10/12 10:58 上午
@@ -13,10 +20,19 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+@Entity
+@Table(name = "todo_items")
 public class TodoItem {
 
+    @Column
     private String content;
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int index;
+
+    @Column
     private boolean done;
 
     public TodoItem(final String content) {
